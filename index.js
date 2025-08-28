6bbf02b8f7395cd5d02d54c03e1b54641d6cfb6f
shimayuu <cnt19024@ict.nitech.ac.jp>
2025-08-28 20:56:03 +0900

    add

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ console.log("nyaaa3");
 console.log("nya3");
 console.log("nyaa3");
 console.log("ny3");
-const a = "nya"
-const b = "nyaa"
-const c = "nyaaa"
-const d = "nyaaaa"
+const a = "nya";
+const b = "nyaa";
+const c = "nyaaa";
+const d = "nyaaaa";
+const nya = "nyaaa";
